9d8f513844317548a627dd9c3de2ca1b9b57c8ed
dannysophi17 <dannysophi17@gmail.com>
2025-08-26 19:10:23 -0500

    process.env.PORT for Render

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require('./index');
+
 const PORT = process.env.PORT || 4000;
 
-const HOST = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1';
-
-app.listen(PORT, HOST, () => {
-  console.log(`🚀 Servidor corriendo en http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
